dd9cde7d6078fb8ab4063e578eb688562fc5b2fb
rbtq <robert.qin@logicmonitor.com>
2016-07-16 17:20:51 +0800

    init

---
+++ platforms/ios/www/js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+  
+.controller('saleTabCtrl', function($scope) {
+
+})
+   
+.controller('storeTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('cloudTabDefaultPageCtrl', function($scope) {
+
+})
+      
+.controller('page5Ctrl', function($scope) {
+
+})
+ 
