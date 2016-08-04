479fadebdf78373836f52c7fc0b8ad3a1283e9bd
rbtq <robert.qin@logicmonitor.com>
2016-08-04 18:01:55 +0800

    add local storage

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('app.controllers', [])
 
-.controller('saleTabCtrl', function($scope, $state, $cordovaBarcodeScanner, SaleService) {
+.controller('saleTabCtrl', function($scope, $state, $cordovaBarcodeScanner, UtilService, SaleService) {
     $scope.today = new Date();
     $scope.monthStart = new Date();
     $scope.monthStart.setDate(1);
@@ -15,57 +15,99 @@ angular.module('app.controllers', [])
     };
 
     updateSaleStatics = function(saleStatics) {
+      UtilService.hideLoading();
       $scope.sale = saleStatics;
     };
 
-    SaleService.getSaleStatics(updateSaleStatics);
+    failedRefreshStatics = function(data, status) {
+      UtilService.hideLoading();
+      UtilService.httpFailed(data, status);
+    };
+
+    refreshSaleStatics = function() {
+      UtilService.showLoading();
+      SaleService.getSaleStatics(updateSaleStatics, failedRefreshStatics);
+    };
+
+    refreshSaleStatics();
 
     $scope.scanBarcode = function() {
-        var imageData = {text:"http://www.tbh.cn/member/product/111224140833"};
-//        $cordovaBarcodeScanner.scan().then(function(imageData) {
+//        var imageData = {text:"http://www.tbh.cn/member/product/111224140833"};
+        $cordovaBarcodeScanner.scan().then(function(imageData) {
             var index = imageData.text.lastIndexOf('/');
             if (index < 0) {
               alert("无效码:"+imageData.text);
               return;
             }
             $state.go('tabs.saleItem', {code:imageData.text.substr(index+1)});
-//        }, function(error) {
-//            alert("扫码失败: " + error);
-//        });
+        }, function(error) {
+            alert("扫码失败: " + error);
+        });
     };
 
     submitSuccess = function(response) {
+      UtilService.hideLoading();
+      $scope.sale.curDay += $scope.saleItems.totalPrice;
+      $scope.sale.curMonth += $scope.saleItems.totalPrice;
       SaleService.clearAll();
-      alert("success");
+      UtilService.showResult("提交成功", true);
+    };
+
+    submitFailed = function(data, status){
+      UtilService.hideLoading();
+      UtilService.httpFailed(data, status);
     };
 
     $scope.submitSaleItems = function() {
-      SaleService.submitSaleItems(submitSuccess);
-    }
+      UtilService.showLoading();
+      SaleService.submitSaleItems(submitSuccess, submitFailed);
+    };
 })
 
-.controller('storeTabCtrl', function($scope, ProductService) {
+.controller('storeTabCtrl', function($scope, ProductService, UtilService) {
     updateStore = function(products) {
+      UtilService.hideLoading();
       $scope.products = products;
     };
 
-    ProductService.getStore(updateStore);
+    failedRefresh = function(data, status) {
+      UtilService.hideLoading();
+      UtilService.httpFailed(data, status);
+    };
+
+    refreshStores = function() {
+      UtilService.showLoading();
+      ProductService.getStore(updateStore, failedRefresh);
+    };
+
+    refreshStores();
 })
 
 .controller('accountTabCtrl', function($scope) {
 
 })
 
-.controller('saleItemCtrl', function($scope, $state, $stateParams, $http, SaleService, ProductService) {
+.controller('saleItemCtrl', function($scope, $state, $stateParams, $http, SaleService, ProductService, UtilService) {
     $scope.descriptions = [];
     updateStore = function(products) {
+      UtilService.hideLoading();
       $scope.stores = products;
       angular.forEach(products, function(product){
         $scope.descriptions.push(product.barCode+"-"+product.title);
       });
     };
 
-    ProductService.getStore(updateStore);
+    failedRefresh = function(data, status) {
+      UtilService.hideLoading();
+      UtilService.httpFailed(data, status);
+    };
+
+    refreshProducts = function() {
+      UtilService.showLoading();
+      ProductService.getStore(updateStore, failedRefresh);
+    };
+
+    refreshProducts();
 
     $scope.selectedProduct = {barCode:0, title:"", unitPrice:0, count:1};
     $scope.selectedStore = {selected:""};
@@ -105,13 +147,20 @@ angular.module('app.controllers', [])
     });
 })
 
-.controller('saleShowCtrl', function($scope, $stateParams, SaleService) {
+.controller('saleShowCtrl', function($scope, $stateParams, UtilService, SaleService) {
     updateSaleDetail = function(saleDetail) {
+      UtilService.hideLoading();
       $scope.saleDetail = saleDetail;
     };
 
+    failedRefresh = function(data, status) {
+      UtilService.hideLoading();
+      UtilService.httpFailed(data, status);
+    };
+
     $scope.getDetail = function(from, to) {
-        SaleService.getDetail(from, to, updateSaleDetail);
+      UtilService.showLoading();
+      SaleService.getDetail(from, to, updateSaleDetail, failedRefresh);
     };
 
     $scope.from = new Date($stateParams.from);
--- www/js/services.js
+++ www/js/services.js
@@ -1,21 +1,20 @@
 angular.module('app.services', [])
 
 .service('SaleService', function(BackgroundService){
-  var sale = {curMonth:123456.00, curDay:321};
-  var saleDetail = [{date:new Date(),totalPrice:1200,items:[{sale:"sq",title:"高压锅",unitPrice:4200,count:1}]},
-  {date:new Date(),totalPrice:1300,items:[{sale:"lq",title:"高压锅",unitPrice:4200,count:1},{sale:"hy",title:"高压锅xxxxx",unitPrice:4200,count:1}]}];
+//  var saleDetail = [{date:new Date(),totalPrice:1200,items:[{sale:"sq",title:"高压锅",unitPrice:4200,count:1}]}];
   var saleItems = {
   totalPrice:0,
   items:[]
   };
 
-  this.getSaleStatics = function(sucCallBack) {
+  this.getSaleStatics = function(sucCallBack, errCallBack) {
         BackgroundService.get("/orders/statics")
              .success(function(response) {
                 response.curMonth /= 100;
                 response.curDay /= 100;
                 sucCallBack(response);
-             });
+             })
+             .error(errCallBack);
     };
 
   this.saleItems = function() {
@@ -50,7 +49,7 @@ angular.module('app.services', [])
         saleItems.items.splice(saleItems.items.indexOf(saleItem), 1);
     };
 
-  this.submitSaleItems = function(sucCallBack) {
+  this.submitSaleItems = function(sucCallBack, errCallBack) {
         var postItem = angular.copy(saleItems);
         postItem.totalPrice *= 100;
         angular.forEach(postItem.items, function(item) {
@@ -60,68 +59,72 @@ angular.module('app.services', [])
         BackgroundService.post("/orders/add", postItem)
               .success(function(response) {
                   sucCallBack(response);
-              });
+              })
+              .error(errCallBack);
     };
 
-  this.getDetail = function(from, to, sucCallBack) {
+  this.getDetail = function(from, to, sucCallBack, errCallBack) {
         BackgroundService.get("/orders/"+from.getTime()+"/"+to.getTime())
-                   .success(function(response) {
-                      var saleDetail = [];
+           .success(function(response) {
+              var saleDetail = [];
 
-                      angular.forEach(response, function(order){
-                        var orderItems = [];
-                        angular.forEach(order.items, function(oi){
-                          oi.unitPrice /= 100;
-                          orderItems.push({sale:order.sale,
-                                           title:oi.title,
-                                           unitPrice:oi.unitPrice,
-                                           count:oi.count});
-                        });
+              angular.forEach(response, function(order){
+                var orderItems = [];
+                angular.forEach(order.items, function(oi){
+                  oi.unitPrice /= 100;
+                  orderItems.push({sale:order.sale,
+                                   title:oi.title,
+                                   unitPrice:oi.unitPrice,
+                                   count:oi.count});
+                });
 
-                        var sdItem = {date:new Date(order.createdAt),
-                                      totalPrice:order.totalPrice,
-                                      items:orderItems};
+                var sdItem = {date:new Date(order.createdAt),
+                              totalPrice:order.totalPrice/100,
+                              items:orderItems};
 
-                        var bFind = false;
-                        for (var i = 0; i < saleDetail.length; i++) {
-                          if (saleDetail[i].date == sdItem.date) {
-                            saleDetail[i].totalPrice += sdItem.totalPrice;
-                            saleDetail[i].items.concat(sdItem.items);
-                            bFind = true;
-                            break;
-                          }
-                        }
+                var bFind = false;
+                for (var i = 0; i < saleDetail.length; i++) {
+                  if (saleDetail[i].date == sdItem.date) {
+                    saleDetail[i].totalPrice += sdItem.totalPrice;
+                    saleDetail[i].items.concat(sdItem.items);
+                    bFind = true;
+                    break;
+                  }
+                }
 
-                        if (!bFind) {
-                          saleDetail.push(sdItem);
-                        }
-                      });
+                if (!bFind) {
+                  saleDetail.push(sdItem);
+                }
+              });
 
-                      sucCallBack(saleDetail);
-                   });
+              sucCallBack(saleDetail);
+           })
+           .error(errCallBack);
     };
 })
 
 .service('ProductService', function(BackgroundService){
 /*  var products = [{barCode:123, title:"高压锅", unitPrice:4200, left:12}];*/
 
-  this.getStore = function(sucCallBack) {
+  this.getStore = function(sucCallBack, errCallBack) {
       BackgroundService.get("/products", { cache: true })
            .success(function(response) {
               angular.forEach(response, function(product){
                 product.unitPrice /= 100;
               });
               sucCallBack(response);
-           });
+           })
+           .error(errCallBack);
   };
 
-  this.getStoreByQR = function(qrCode, sucCallBack) {
+  this.getStoreByQR = function(qrCode, sucCallBack, errCallBack) {
       if (angular.isDefined(qrCode) && (!(null === qrCode)) && ("" != qrCode)){
             BackgroundService.get("/products/qr/"+qrCode, { cache: true })
                .success(function(response) {
                   response.unitPrice /= 100;
                   sucCallBack(response);
-               });
+               })
+               .error(errCallBack);
       }
   };
 })
@@ -138,6 +141,70 @@ angular.module('app.services', [])
 
   this.post = function(url, data) {
     return $http.post(backend+url, data);
+  };
+})
+
+.service('UtilService', function($ionicPopup, $ionicLoading) {
+  this.showResult = function(message, success) {
+    var temp = '<a class="item-icon-left">'
+    if (success) {
+      temp += '<i class="icon ion-checkmark-round positive"></i>'+message+'</a>';
+    } else {
+      temp += '<i class="icon ion-android-close assertive"></i>'+message+'</a>'
+    }
+
+    $ionicPopup.alert({
+                       title:'提示',
+                       template:temp
+                     });
+  };
+
+  this.httpFailed = function(data,status){
+    if (400 == status) {
+      this.showResult(data, false);
+    } else if (404 == status) {
+      this.showResult("内部错误", false);
+    } else if (0 == status) {
+      this.showResult("连接服务器失败", false);
+    }else {
+      this.showResult("未知错误", false);
+    }
+  };
+
+  this.showLoading = function() {
+    $ionicLoading.show({
+              content: 'Loading',
+              animation: 'fade-in',
+              showBackdrop: true,
+              maxWidth: 200,
+              showDelay: 0
+            });
+  };
+
+  this.hideLoading = function() {
+    $ionicLoading.hide();
+  };
+})
+
+.service('locals', function($window){
+  //存入单个属性
+  this.set = function(key,value){
+    $window.localStorage[key]=value;
+  };
+  
+  //读取单个属性
+  this.get = function(key,defaultValue){
+    return  $window.localStorage[key] || defaultValue;
+  };
+
+  //存储对象，以JSON格式存储
+  this.setObject = function(key, value){
+    $window.localStorage[key]=JSON.stringify(value);
+  };
+
+  //读取对象
+  this.getObject = function(key) {
+    return JSON.parse($window.localStorage[key] || '{}');
   };
 });
 
